89ff9ae7dd8c0ebe467856d78e2fa17511007f8a
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-08-09 19:26:08 +0200

    Output data from database.

--- app.js
+++ app.js
@@ -25,57 +25,6 @@ app.use('/users', usersRouter);
 
 
 
-const mongoose = require("mongoose");
-
-// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
-// Included because it removes preparatory warnings for Mongoose 7.
-// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-mongoose.set("strictQuery", false);
-const Schema = mongoose.Schema
-const testSchema = new Schema({
-  name: {
-    type: String,
-    min: 2,
-    max: 10,
-    required: true, 
-  }, 
-  birthday: Date,
-  alive: Boolean, 
-  friends: [], 
-})
-const MongooseModel = mongoose.model("Friends", testSchema)
-
-
-
-async function addFriendToCollection(name){
-  const newFriend = new MongooseModel({ 
-    name: name, 
-    birthday: new Date(),
-    alive: true,
-  });
-  newFriend
-    .save()
-    .then(
-      () => console.log(`${name} was added to your friend collection. `),
-      (err) => console.log(err)
-    )
-}
-
-addFriendToCollection('Yujeong')
-
-
-
-
-
-
-// Define the database URL to connect to.
-const mongoDB = process.env.MONGODB_URL
-// Wait for database to connect, logging an error if there is a problem
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-}
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+require("dotenv").config()
+const mongoose = require("mongoose");
+const app = require('../app');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+mongoose.set("strictQuery", false);
+const Schema = mongoose.Schema
+const testSchema = new Schema({
+  name: {
+    type: String,
+    min: 2,
+    max: 10,
+    required: true, 
+  }, 
+  birthday: Date,
+  alive: Boolean, 
+  friends: [], 
+})
+const FriendModel = mongoose.model("Friends", testSchema)
+
+
+
+async function addFriendToCollection(name){
+  const newFriend = new FriendModel({ 
+    name: name, 
+    birthday: new Date(),
+    alive: true,
+  });
+  newFriend
+    .save()
+    .then(
+      () => console.log(`${name} was added to your friend collection. `),
+      (err) => console.log(err)
+    )
+}
+
+
+// Define the database URL to connect to.
+const mongoDB = process.env.MONGODB_URL
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+}
+router.get('/', async (req, res) => {
+  try{
+    const friendsFromDB = await FriendModel.find({})
+    res.send(friendsFromDB)
+  } catch(err){
+    console.log(err)
+  }
+
 });
-
-
-
 
 module.exports = router;
